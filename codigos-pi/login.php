a13eb19eee86ec116f06c4e764494771452b9016
Pedro Benevides Souza <pedrobenevidessouza77@gmail.com>
2025-10-21 19:27:29 -0300

    adicionando bootstrap

--- codigos-pi/login.php
+++ codigos-pi/login.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 $mensagem_erro = ''; 
 
 if (isset($_GET['erro'])) {
@@ -6,13 +6,14 @@ if (isset($_GET['erro'])) {
         $mensagem_erro = "Usuário ou senha inválidos. Tente novamente.";
     }
 }
-?>
+?>  -->
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Formulário de Cadastro</title>
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
